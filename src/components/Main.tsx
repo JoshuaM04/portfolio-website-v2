ee38a852b60892a0daf24275a74f285808a0b90d
Viper <pruncle21@gmail.com>
2026-06-13 13:51:59 -0500

    Added various clamp() functions in the .css file in order to create a responsive interface across different resolutions. These clamp() functions are synchronized across various containers pertaining to each section of the webpage (e.g. personal-overview, featured-projects, featured-experience, and contact-information). The h2 elements are also in-sync with the width clamp() function so they remain left aligned and no weird visual asymmetrical sliding takes place. A clamp() function is also applied for the padding-top and padding-bottom of the hero section so the heading is not way off-center on mobile resolutions. Lastly, a clamp() function is applied to the personal-overview-text and personal-overview-skills. Any other changes pertaining to adapting the container layouts to this new layout in order for content to remain aligned and evenly spaced.

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,7 +6,7 @@ import {Button} from 'react-aria-components/Modal';
 export default function Main() {
     return (
         <main className="bg-orange-100">
-            <section id="hero" className="bg-linear-to-b from-orange-100 to-red-300 flex justify-center items-center flex-wrap gap-50 pt-110 pb-110 pl-5 pr-5">
+            <section id="hero" className="hero-container bg-linear-to-b from-orange-100 to-red-300 flex justify-center items-center flex-wrap gap-50 pl-5 pr-5">
                 <div id="career-overview" className="flex flex-col gap-5">
                     <p className="font-semibold text-indigo-400">Software Engineer</p>
                     <h1 className="text-8xl">Joshua Martinez</h1>
@@ -41,11 +41,11 @@ export default function Main() {
                 </div>
             </section>
 
-            <section id="personal-overview" className="flex flex-col special:items-center gap-10 pt-50 pb-50 pl-5 pr-5">
-                <h2 className="text-6xl font-bold w-270 max-special:w-125">Personal Overview</h2>
+            <section id="personal-overview" className="flex flex-col items-center gap-10 pt-50 pb-50 pl-5 pr-5">
+                <h2 className="text-6xl font-bold">Personal Overview</h2>
 
-                <div className="flex flex-wrap gap-20">
-                    <div className="flex flex-col gap-5 w-125">
+                <div className="personal-overview-container flex flex-wrap gap-20">
+                    <div className="personal-overview-text flex flex-col gap-5">
                         <p className="text-slate-500">
                             I'm a <span className="font-bold">recent graduate</span> Software Engineer specializing in building accessible, responsive web applications using
                             HTML, CSS, TypeScript, React, and Tailwind CSS, with a strong focus on WCAG
@@ -60,7 +60,7 @@ export default function Main() {
                         </p>
                     </div>
 
-                    <div className="flex flex-col gap-5 bg-white border border-red-200 rounded-sm shadow-lg p-5 h-min w-125">
+                    <div className="personal-overview-skills flex flex-col gap-5 bg-white border border-red-200 rounded-sm shadow-lg p-5 h-min">
                         <div className="bg-linear-to-b from-orange-200 to-red-200 p-5 -ml-5 -mr-5 -mt-5">
                             <p className="text-red-900 font-semibold">Core Competencies</p>
                         </div>
@@ -79,10 +79,10 @@ export default function Main() {
             </section>
 
             <section id="projects" className="flex flex-col items-center gap-10 pt-50 pb-50 pl-5 pr-5">
-                <h2 className="text-6xl font-bold w-270">Featured Projects</h2>
-
-                <div className="flex flex-wrap gap-10 w-270">
-                    <div className="bg-white rounded-sm w-120 shadow-sm transition-all duration-1000 ease-out hover:translate-y-[-3%] hover:shadow-xl hover:cursor-pointer">
+                <div className="featured-projects-container flex flex-wrap gap-10">
+                    <h2 className="text-6xl font-bold">Featured Projects</h2>
+                    
+                    <div className="featured-project bg-white rounded-sm w-120 shadow-sm transition-all duration-1000 ease-out hover:translate-y-[-3%] hover:shadow-xl hover:cursor-pointer">
                         <a href="https://loop-studios-landing-page-kohl.vercel.app/"><img className="rounded-t-sm border-b-2 border-red-400" src="/loopStudiosDesktop.png" alt="" /></a>
                         
                         <div className="flex flex-col gap-5 p-5">
@@ -106,7 +106,7 @@ export default function Main() {
                         </div>
                     </div>
 
-                    <div className="bg-white rounded-sm w-120 shadow-sm transition-all duration-1000 ease-out hover:translate-y-[-3%] hover:shadow-xl hover:cursor-pointer">
+                    <div className="featured-project bg-white rounded-sm w-120 shadow-sm transition-all duration-1000 ease-out hover:translate-y-[-3%] hover:shadow-xl hover:cursor-pointer">
                         <a href="https://tip-calculator-app-brown-three.vercel.app/"><img className="rounded-t-sm border-b-2 border-red-400" src="/tipCalculatorDesktop.png" alt="" /></a>
 
                         <div className="flex flex-col gap-5 p-5">
@@ -130,7 +130,7 @@ export default function Main() {
                         </div>
                     </div>
 
-                    <div className="bg-white rounded-sm w-120 shadow-sm transition-all duration-1000 ease-out hover:translate-y-[-3%] hover:shadow-xl hover:cursor-pointer">
+                    <div className="featured-project bg-white rounded-sm w-120 shadow-sm transition-all duration-1000 ease-out hover:translate-y-[-3%] hover:shadow-xl hover:cursor-pointer">
                         <a href="https://contact-form-eosin-psi.vercel.app/"><img className="rounded-t-sm border-b-2 border-red-400" src="/contactFormDesktop.png" alt="" /></a>
 
                         <div className="flex flex-col gap-5 p-5">
@@ -153,7 +153,7 @@ export default function Main() {
                         </div>
                     </div>
 
-                    <div className="bg-white rounded-sm w-120 shadow-sm transition-all duration-1000 ease-out hover:translate-y-[-3%] hover:shadow-xl hover:cursor-pointer">
+                    <div className="featured-project bg-white rounded-sm w-120 shadow-sm transition-all duration-1000 ease-out hover:translate-y-[-3%] hover:shadow-xl hover:cursor-pointer">
                         <a href="https://time-tracking-dashboard-zeta-plum.vercel.app/"><img className="rounded-t-sm border-b-2 border-red-400" src="/timeTrackingDashboardDesktop.png" alt="" /></a>
 
                         <div className="flex flex-col gap-5 p-5">
@@ -177,7 +177,7 @@ export default function Main() {
                         </div>
                     </div>
 
-                    <div className="bg-white rounded-sm w-120 shadow-sm transition-all duration-1000 ease-out hover:translate-y-[-3%] hover:shadow-xl hover:cursor-pointer">
+                    <div className="featured-project bg-white rounded-sm w-120 shadow-sm transition-all duration-1000 ease-out hover:translate-y-[-3%] hover:shadow-xl hover:cursor-pointer">
                         <a href="https://news-homepage-seven-sooty.vercel.app/"><img className="rounded-t-sm border-b-2 border-red-400" src="/newsHomepageDesktop.png" alt="" /></a>
 
                         <div className="flex flex-col gap-5 p-5">
@@ -200,7 +200,7 @@ export default function Main() {
                         </div>
                     </div>
 
-                    <div className="bg-white rounded-sm w-120 shadow-sm transition-all duration-1000 ease-out hover:translate-y-[-3%] hover:shadow-xl hover:cursor-pointer">
+                    <div className="featured-project bg-white rounded-sm w-120 shadow-sm transition-all duration-1000 ease-out hover:translate-y-[-3%] hover:shadow-xl hover:cursor-pointer">
                         <a href="https://interactive-rating-component-azure-seven.vercel.app/"><img className="rounded-t-sm border-b-2 border-red-400" src="/interactiveRatingComponentDesktop.png" alt="" /></a>
 
                         <div className="flex flex-col gap-5 p-5">
@@ -228,10 +228,10 @@ export default function Main() {
             </section>
 
             <section id="experience" className="flex flex-col items-center gap-10 pt-50 pb-50 pl-5 pr-5">
-                <h2 className="text-6xl font-bold w-270">Experience</h2>
+                <div className="featured-experience-container flex flex-wrap gap-10">
+                    <h2 className="text-6xl font-bold">Experience</h2>
 
-                <div className="w-270">
-                    <div className="flex flex-col gap-5 bg-white border border-red-200 p-5 w-100 shadow-lg rounded-sm">
+                    <div className="featured-experience flex flex-col gap-5 bg-white border border-red-200 p-5 shadow-lg rounded-sm">
                         <div className="bg-linear-to-b from-orange-200 to-red-200 p-5 -ml-5 -mr-5 -mt-5">
                             <p className="text-red-900 text-lg font-bold">Stellar AI</p>
                             <p className="font-semibold text-red-500/60">AI Tester and Validation</p>
@@ -260,21 +260,24 @@ export default function Main() {
             </section>
 
             <section id="contact" className="flex flex-col items-center gap-10 pt-50 pb-50 pl-5 pr-5">
-                <h2 className="text-6xl font-bold w-270">Contact</h2>
+                <div className="contact-information-container flex flex-wrap gap-10">
+                    <h2 className="text-6xl font-bold">Contact</h2>
 
-                <div className="flex flex-col gap-5 w-270">
-                    <p className="uppercase">Email:</p>
-                    <p className="uppercase">Phone:</p>
-                    <p className="uppercase">Location:</p>
-                </div>
-
-                <div className="flex gap-5 w-270">
-                    <a href="">
-                        <svg fill="#000000" width="40px" height="40px" viewBox="0 0 24.00 24.00" xmlns="http://www.w3.org/2000/svg" stroke="#000000" strokeWidth="0.00024000000000000003" transform="rotate(0)"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="#CCCCCC" strokeWidth="0.192"></g><g id="SVGRepo_iconCarrier"> <g data-name="Layer 2"> <rect width="24" height="24" transform="rotate(180 12 12)" opacity="0"></rect> <path d="M12 1A10.89 10.89 0 0 0 1 11.77 10.79 10.79 0 0 0 8.52 22c.55.1.75-.23.75-.52v-1.83c-3.06.65-3.71-1.44-3.71-1.44a2.86 2.86 0 0 0-1.22-1.58c-1-.66.08-.65.08-.65a2.31 2.31 0 0 1 1.68 1.11 2.37 2.37 0 0 0 3.2.89 2.33 2.33 0 0 1 .7-1.44c-2.44-.27-5-1.19-5-5.32a4.15 4.15 0 0 1 1.11-2.91 3.78 3.78 0 0 1 .11-2.84s.93-.29 3 1.1a10.68 10.68 0 0 1 5.5 0c2.1-1.39 3-1.1 3-1.1a3.78 3.78 0 0 1 .11 2.84A4.15 4.15 0 0 1 19 11.2c0 4.14-2.58 5.05-5 5.32a2.5 2.5 0 0 1 .75 2v2.95c0 .35.2.63.75.52A10.8 10.8 0 0 0 23 11.77 10.89 10.89 0 0 0 12 1" data-name="github"></path> </g> </g></svg>
-                    </a>
-                    <a href="">
-                        <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M18.72 3.99997H5.37C5.19793 3.99191 5.02595 4.01786 4.86392 4.07635C4.70189 4.13484 4.55299 4.22471 4.42573 4.34081C4.29848 4.45692 4.19537 4.59699 4.12232 4.75299C4.04927 4.909 4.0077 5.07788 4 5.24997V18.63C4.01008 18.9901 4.15766 19.3328 4.41243 19.5875C4.6672 19.8423 5.00984 19.9899 5.37 20H18.72C19.0701 19.9844 19.4002 19.8322 19.6395 19.5761C19.8788 19.32 20.0082 18.9804 20 18.63V5.24997C20.0029 5.08247 19.9715 4.91616 19.9078 4.76122C19.8441 4.60629 19.7494 4.466 19.6295 4.34895C19.5097 4.23191 19.3672 4.14059 19.2108 4.08058C19.0544 4.02057 18.8874 3.99314 18.72 3.99997ZM9 17.34H6.67V10.21H9V17.34ZM7.89 9.12997C7.72741 9.13564 7.5654 9.10762 7.41416 9.04768C7.26291 8.98774 7.12569 8.89717 7.01113 8.78166C6.89656 8.66615 6.80711 8.5282 6.74841 8.37647C6.6897 8.22474 6.66301 8.06251 6.67 7.89997C6.66281 7.73567 6.69004 7.57169 6.74995 7.41854C6.80986 7.26538 6.90112 7.12644 7.01787 7.01063C7.13463 6.89481 7.2743 6.80468 7.42793 6.74602C7.58157 6.68735 7.74577 6.66145 7.91 6.66997C8.07259 6.66431 8.2346 6.69232 8.38584 6.75226C8.53709 6.8122 8.67431 6.90277 8.78887 7.01828C8.90344 7.13379 8.99289 7.27174 9.05159 7.42347C9.1103 7.5752 9.13699 7.73743 9.13 7.89997C9.13719 8.06427 9.10996 8.22825 9.05005 8.3814C8.99014 8.53456 8.89888 8.6735 8.78213 8.78931C8.66537 8.90513 8.5257 8.99526 8.37207 9.05392C8.21843 9.11259 8.05423 9.13849 7.89 9.12997ZM17.34 17.34H15V13.44C15 12.51 14.67 11.87 13.84 11.87C13.5822 11.8722 13.3313 11.9541 13.1219 12.1045C12.9124 12.2549 12.7546 12.4664 12.67 12.71C12.605 12.8926 12.5778 13.0865 12.59 13.28V17.34H10.29V10.21H12.59V11.21C12.7945 10.8343 13.0988 10.5225 13.4694 10.3089C13.84 10.0954 14.2624 9.98848 14.69 9.99997C16.2 9.99997 17.34 11 17.34 13.13V17.34Z" fill="#000000"></path> </g></svg>
-                    </a>
+                    <div className="flex flex-col gap-5">
+                        <div className="flex flex-col gap-5">
+                            <p className="uppercase">Email:</p>
+                            <p className="uppercase">Phone:</p>
+                            <p className="uppercase">Location:</p>
+                        </div>
+                        <div className="flex gap-5">
+                            <a href="">
+                                <svg fill="#000000" width="40px" height="40px" viewBox="0 0 24.00 24.00" xmlns="http://www.w3.org/2000/svg" stroke="#000000" strokeWidth="0.00024000000000000003" transform="rotate(0)"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="#CCCCCC" strokeWidth="0.192"></g><g id="SVGRepo_iconCarrier"> <g data-name="Layer 2"> <rect width="24" height="24" transform="rotate(180 12 12)" opacity="0"></rect> <path d="M12 1A10.89 10.89 0 0 0 1 11.77 10.79 10.79 0 0 0 8.52 22c.55.1.75-.23.75-.52v-1.83c-3.06.65-3.71-1.44-3.71-1.44a2.86 2.86 0 0 0-1.22-1.58c-1-.66.08-.65.08-.65a2.31 2.31 0 0 1 1.68 1.11 2.37 2.37 0 0 0 3.2.89 2.33 2.33 0 0 1 .7-1.44c-2.44-.27-5-1.19-5-5.32a4.15 4.15 0 0 1 1.11-2.91 3.78 3.78 0 0 1 .11-2.84s.93-.29 3 1.1a10.68 10.68 0 0 1 5.5 0c2.1-1.39 3-1.1 3-1.1a3.78 3.78 0 0 1 .11 2.84A4.15 4.15 0 0 1 19 11.2c0 4.14-2.58 5.05-5 5.32a2.5 2.5 0 0 1 .75 2v2.95c0 .35.2.63.75.52A10.8 10.8 0 0 0 23 11.77 10.89 10.89 0 0 0 12 1" data-name="github"></path> </g> </g></svg>
+                            </a>
+                            <a href="">
+                                <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M18.72 3.99997H5.37C5.19793 3.99191 5.02595 4.01786 4.86392 4.07635C4.70189 4.13484 4.55299 4.22471 4.42573 4.34081C4.29848 4.45692 4.19537 4.59699 4.12232 4.75299C4.04927 4.909 4.0077 5.07788 4 5.24997V18.63C4.01008 18.9901 4.15766 19.3328 4.41243 19.5875C4.6672 19.8423 5.00984 19.9899 5.37 20H18.72C19.0701 19.9844 19.4002 19.8322 19.6395 19.5761C19.8788 19.32 20.0082 18.9804 20 18.63V5.24997C20.0029 5.08247 19.9715 4.91616 19.9078 4.76122C19.8441 4.60629 19.7494 4.466 19.6295 4.34895C19.5097 4.23191 19.3672 4.14059 19.2108 4.08058C19.0544 4.02057 18.8874 3.99314 18.72 3.99997ZM9 17.34H6.67V10.21H9V17.34ZM7.89 9.12997C7.72741 9.13564 7.5654 9.10762 7.41416 9.04768C7.26291 8.98774 7.12569 8.89717 7.01113 8.78166C6.89656 8.66615 6.80711 8.5282 6.74841 8.37647C6.6897 8.22474 6.66301 8.06251 6.67 7.89997C6.66281 7.73567 6.69004 7.57169 6.74995 7.41854C6.80986 7.26538 6.90112 7.12644 7.01787 7.01063C7.13463 6.89481 7.2743 6.80468 7.42793 6.74602C7.58157 6.68735 7.74577 6.66145 7.91 6.66997C8.07259 6.66431 8.2346 6.69232 8.38584 6.75226C8.53709 6.8122 8.67431 6.90277 8.78887 7.01828C8.90344 7.13379 8.99289 7.27174 9.05159 7.42347C9.1103 7.5752 9.13699 7.73743 9.13 7.89997C9.13719 8.06427 9.10996 8.22825 9.05005 8.3814C8.99014 8.53456 8.89888 8.6735 8.78213 8.78931C8.66537 8.90513 8.5257 8.99526 8.37207 9.05392C8.21843 9.11259 8.05423 9.13849 7.89 9.12997ZM17.34 17.34H15V13.44C15 12.51 14.67 11.87 13.84 11.87C13.5822 11.8722 13.3313 11.9541 13.1219 12.1045C12.9124 12.2549 12.7546 12.4664 12.67 12.71C12.605 12.8926 12.5778 13.0865 12.59 13.28V17.34H10.29V10.21H12.59V11.21C12.7945 10.8343 13.0988 10.5225 13.4694 10.3089C13.84 10.0954 14.2624 9.98848 14.69 9.99997C16.2 9.99997 17.34 11 17.34 13.13V17.34Z" fill="#000000"></path> </g></svg>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </section>
         </main>
